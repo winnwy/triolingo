01e3c873d430d1dca3e09964dacd709c10b7b53f
winnie <winnwyz@gmail.com>
2025-03-10 14:23:58 +1100

    Add authentication

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -9,7 +9,7 @@ const MarketingLayout = ({ children }: Props) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 flex flex-col items-center justify-center">
+      <main className="flex-1 flex flex-col items-center justify-center ">
         {children}
       </main>
       <Footer />
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,54 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/clerk-react";
+import { ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
-  return <div>
-    Marketing page
-  </div>;
+  return (
+    <div
+      className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center
+    justify-center p-4 gap-2"
+    >
+      <div className="relative size-[240px] lg:size-[424px] mb-8 lg:mb-0">
+        <Image src="../images/hero.svg" fill alt="Hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[490px] text-center">
+          Learn, practice, and master new languages with triolingo.
+        </h1>
+        <div>
+          <ClerkLoading>
+            <Loader className="size-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton>
+                <Button size="lg" variant="secondary" className="w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton mode="modal">
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/learn">Continue Learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
+    </div>
+  );
 }

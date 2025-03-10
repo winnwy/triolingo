635f51098bdf1a6b5c918c0acae1aaad5d5a8dd4
winnie <winnwyz@gmail.com>
2025-03-10 20:15:06 +1100

    Add learn page wrappers

---
+++ components/user-progress.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { InfinityIcon } from "lucide-react";
+
+type Props = {
+  activeCourse: { imageSrc: string; title: string };
+  hearts: number;
+  points: number;
+  hasActiveSubscription: boolean;
+};
+
+export const UserProgress = ({
+  activeCourse,
+  hearts,
+  points,
+  hasActiveSubscription,
+}: Props) => {
+  return (
+    <div className="flex items-center justify-between gap-x-2 w-full">
+      <Link href="/courses">
+        <Button variant="ghost">
+          <Image
+            src={activeCourse.imageSrc}
+            alt={activeCourse.title}
+            width={32}
+            height={32}
+            className="rounded-md border"
+          />
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-orange-500">
+          <Image
+            src="../images/points.svg"
+            height={28}
+            width={28}
+            alt="Points"
+            className="mr-2"
+          />
+          {points}
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-rose-500">
+          <Image
+            src="../images/heart.svg"
+            height={22}
+            width={22}
+            alt="Hearts"
+            className="mr-2"
+          />
+          {hasActiveSubscription ? (
+            <InfinityIcon className="size-4 stroke-3" />
+          ) : (
+            hearts
+          )}
+        </Button>
+      </Link>
+    </div>
+  );
+};

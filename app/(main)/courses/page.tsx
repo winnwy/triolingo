e88af932f3284fbeccc5d5b16cc7a1ca3aa54f81
winnie <winnwyz@gmail.com>
2025-03-11 14:20:34 +1100

    set up user progress

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,15 +1,16 @@
-import { getCourses } from "@/db/queries";
+import { getCourses, getUserProgress } from "@/db/queries";
 import { List } from "./list";
 
 const CoursePage = async () => {
   const courses = await getCourses();
+  const userProgress = await getUserProgress();
 
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
       <List
         courses={courses}
-        activeCourseId={1}
+        activeCourseId={userProgress?.activeCourseId}
       />
     </div>
   );

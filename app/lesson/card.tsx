82c4b705844afd5a965938773ba947081eb79877
winnie <winnwyz@gmail.com>
2025-03-12 16:13:26 +1100

    Add challenge footer for wrong, correct and completed

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -19,7 +19,6 @@ type Props = {
 };
 
 export const Card = ({
-  id,
   imageSrc,
   audioSrc,
   text,
@@ -30,7 +29,7 @@ export const Card = ({
   disabled,
   type,
 }: Props) => {
-  const [audio, _, controls] = useAudio({ src: audioSrc || "" });
+  const [audio, , controls] = useAudio({ src: audioSrc || "" });
 
   const handleClick = useCallback(() => {
     if (disabled) return;
--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Header } from "./header";
 import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
+import { Footer } from "./footer";
 
 type Props = {
   initialPercentage: number;
@@ -82,6 +83,11 @@ export const Quiz = ({
           </div>
         </div>
       </div>
+      <Footer
+        disabled={!selectedOption}
+        status={status}
+        onCheck={()=>{}}
+      />
     </>
   );
 };

f7418625683f0462f5400581756e052dec71f7f3
winnie <winnwyz@gmail.com>
2025-03-12 14:34:33 +1100

    Add header for lesson

---
+++ app/lesson/quiz.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useState} from "react";
+import { Header } from "./header";
+
+type Props ={
+  initialPercentage: number;
+  initialHearts: number;
+  initialLessonId: number;
+  initialLessonChallenges: (typeof challenges.$inferSelect & {
+    completed: boolean;
+    challengeOptions: typeof challengeOptions.$inferSelect[];
+  })[];
+  userSubscription: typeof userSubscription.$inferSelect & {
+    isActive: boolean;
+  } | null;
+};
+
+export const Quiz = ({
+  initialPercentage,
+  initialHearts,
+  initialLessonId,
+  initialLessonChallenges,
+  userSubscription,
+}: Props) => {
+    const [hearts, setHearts] = useState(initialHearts);
+    const [percentage, setPercentage] = useState(initialPercentage);
+
+  return (
+    <>
+      <Header
+        hearts={hearts}
+        percentage={percentage}
+        hasActiveSubscription={!!userSubscription?.isActive}
+      />
+    </>
+  );
+};

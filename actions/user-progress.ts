d0251bcecc54307959010181ccff7735a8d47275
winnie <winnwyz@gmail.com>
2025-03-12 16:38:54 +1100

    Add next question for correct answers

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -1,14 +1,11 @@
 "use server";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import db from "@/db/drizzle";
-import {
-  getCourseById,
-  getUserProgress,
-} from "@/db/queries";
-import { userProgress } from "@/db/schema";
+import { getCourseById, getUserProgress } from "@/db/queries";
+import { challengeProgress, challenges, userProgress} from "@/db/schema";
 import { auth, currentUser } from "@clerk/nextjs/server";
 
 export const upsertUserProgress = async (courseId: number) => {
@@ -21,8 +18,8 @@ export const upsertUserProgress = async (courseId: number) => {
 
   if (!course) throw new Error("Course not found.");
 
-//   if (!course.units.length || !course.units[0].lessons.length)
-//     throw new Error("Course is empty.");
+  //   if (!course.units.length || !course.units[0].lessons.length)
+  //     throw new Error("Course is empty.");
 
   const existingUserProgress = await getUserProgress();
 
@@ -51,4 +48,58 @@ export const upsertUserProgress = async (courseId: number) => {
   revalidatePath("/courses");
   revalidatePath("/learn");
   redirect("/learn");
+};
+
+export const reduceHearts = async (challengeId: number) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const currentUserProgress = await getUserProgress();
+
+  const challenge = await db.query.challenges.findFirst({
+    where: eq(challenges.id, challengeId),
+  });
+
+  if (!challenge) {
+    throw new Error("Challenge not found");
+  }
+
+  const lessonId = challenge.lessonId;
+
+  const existingChallengeProgress = await db.query.challengeProgress.findFirst({
+    where: and(
+      eq(challengeProgress.userId, userId),
+      eq(challengeProgress.challengeId, challengeId)
+    ),
+  });
+
+  const isPractice = !!existingChallengeProgress;
+
+  if (isPractice) {
+    return { error: "practice" };
+  }
+
+  if (!currentUserProgress) {
+    throw new Error("User progress not found");
+  }
+
+  if (currentUserProgress.hearts === 0) {
+    return { error: "hearts" };
+  }
+
+  await db
+    .update(userProgress)
+    .set({
+      hearts: Math.max(currentUserProgress.hearts - 1, 0),
+    })
+    .where(eq(userProgress.userId, userId));
+
+  revalidatePath("/shop");
+  revalidatePath("/learn");
+  revalidatePath("/quests");
+  revalidatePath("/leaderboard");
+  revalidatePath(`/lesson/${lessonId}`);
 };
--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import { Header } from "./header";
 import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
 import { Footer } from "./footer";
+import { upsertChallengeProgress } from "@/actions/challenge-progress";
+import { toast } from "sonner";
+import { challengeOptions, challenges, userSubscription } from "@/db/schema";
 
 type Props = {
   initialPercentage: number;
@@ -27,6 +30,7 @@ export const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
+  const [pending, startTransition] = useTransition();
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
   const [challenges] = useState(initialLessonChallenges);
@@ -43,11 +47,62 @@ export const Quiz = ({
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
 
+    const onNext = () => {
+    setActiveIndex((current) => current + 1);
+  }; 
+
   const onSelect = (id: number) => {
     if (status !== "none") return;
 
     setSelectedOption(id);
   };
+
+  const onContinue = () => {
+    if (!selectedOption) return;
+
+    if (status === "wrong") {
+      setStatus("none");
+      setSelectedOption(undefined);
+      return;
+    }
+
+    if (status === "correct") {
+      onNext();
+      setStatus("none");
+      setSelectedOption(undefined);
+      return;
+    }
+    
+    const correctOption = options.find((option) => option.correct);
+
+    if (!correctOption) {
+      return;
+    }
+
+    if (correctOption.id === selectedOption) {
+      startTransition(() => {
+        upsertChallengeProgress(challenge.id)
+          .then((response) => {
+            if (response?.error === "hearts") {
+              // openHeartsModal();
+              return;
+            }
+
+            // correctControls.play();
+            setStatus("correct");
+            setPercentage((prev) => prev + 100 / challenges.length);
+
+            // This is a practice
+            if (initialPercentage === 100) {
+              setHearts((prev) => Math.min(prev + 1, 5));
+            }
+          })
+          .catch(() => toast.error("Something went wrong. Please try again."))
+      });
+    } else {
+    }
+  };
+
 
   const title =
     challenge.type === "ASSIST"
@@ -76,7 +131,7 @@ export const Quiz = ({
                 onSelect={onSelect}
                 status={status}
                 selectedOption={selectedOption}
-                disabled={false}
+                disabled={pending}
                 type={challenge.type}
               />
             </div>
@@ -84,9 +139,9 @@ export const Quiz = ({
         </div>
       </div>
       <Footer
-        disabled={!selectedOption}
+        disabled={pending || !selectedOption}
         status={status}
-        onCheck={()=>{}}
+        onCheck={onContinue}
       />
     </>
   );
